0be9d8a8085c4613887163782b827ff12965e7a6
Harvey <yerik05vh@gmail.com>
2022-10-29 15:37:46 -0500

    add only number two input

--- src/components/AppComponent.tsx
+++ src/components/AppComponent.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { cloneElement, useEffect, useState } from "react";
 import logo from "../assets/logo.svg";
 import dollar from "../assets/icon-dollar.svg";
 import person from "../assets/icon-person.svg";
@@ -25,64 +25,222 @@ import {
   ATPartOneBoxOneDescription,
   ATPartOneBoxTwoPrice,
   AppTwoPartEnd,
+  AppAll,
+  AppCenter,
+  PartTwoButtonActive,
 } from "./Styles";
 
 export default function AppComponent() {
+  type tipPor = {
+    numberSelect: number;
+    active: boolean;
+  };
+  const initialState: tipPor[] = [
+    {
+      numberSelect: 5,
+      active: false,
+    },
+    {
+      numberSelect: 10,
+      active: false,
+    },
+    {
+      numberSelect: 15,
+      active: false,
+    },
+    {
+      numberSelect: 25,
+      active: false,
+    },
+    {
+      numberSelect: 50,
+      active: false,
+    },
+  ];
+
+  const [tipRender, setTipRender] = useState<tipPor[]>([]);
+  const [billState, setBillState] = useState<string>();
+  const [theState, setTheState] = useState<string[]>([]);
+
+  const [peopleState, setPeopleState] = useState<string>();
+  const [theStatePeople, setTheStatePeople] = useState<string[]>([]);
+  const handleClick = (id: number) => {
+    let moreVar = resetStatusTip();
+    const varTemp: tipPor[] = [...moreVar];
+    let varfind: tipPor | undefined = varTemp.find(
+      (item) => item.numberSelect === id
+    );
+    if (varfind !== undefined) {
+      varfind.active = !varfind.active;
+    }
+    setTipRender(varTemp);
+  };
+  const handleKey = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    let onlyNumbers: string[] = [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "0",
+      ".",
+    ];
+    console.log(billState);
+    if (onlyNumbers.includes(e.key)) {
+      let stateTemp = [...theState, e.key];
+      setTheState(stateTemp);
+      let stateConvert: string = theState.join("");
+      setBillState(stateConvert);
+    } else {
+      let stateConvert: string = theState.join("");
+      setBillState(stateConvert);
+    }
+
+    if (e.key == "Backspace") {
+      let stateTemp = theState.pop();
+      let otherTemp = [...theState];
+      let filterVar = otherTemp.filter((item: string) => item !== stateTemp);
+      setTheState(filterVar);
+
+      let stateConvert: string = theState.join("");
+      setBillState(stateConvert);
+    }
+  };
+  const handleKeyTwo = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    let onlyNumbers: string[] = [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "0",
+      ".",
+    ];
+    console.log(peopleState);
+    if (onlyNumbers.includes(e.key)) {
+      let stateTemp = [...theStatePeople, e.key];
+      setTheStatePeople(stateTemp);
+      let stateConvert: string = theStatePeople.join("");
+      setPeopleState(stateConvert);
+    } else {
+      let stateConvert: string = theStatePeople.join("");
+      setPeopleState(stateConvert);
+    }
+
+    if (e.key == "Backspace") {
+      let stateTemp = theStatePeople.pop();
+      let otherTemp = [...theStatePeople];
+      let filterVar = otherTemp.filter((item: string) => item !== stateTemp);
+      setTheStatePeople(filterVar);
+
+      let stateConvert: string = theStatePeople.join("");
+      setPeopleState(stateConvert);
+    }
+  };
+  function resetStatusTip(): tipPor[] {
+    const moreVar = [...tipRender];
+    moreVar.map((item) => {
+      if (item.active) {
+        item.active = false;
+      }
+    });
+    setTipRender(moreVar);
+    return moreVar;
+  }
+
+  useEffect(() => {
+    setTipRender(initialState);
+  }, []);
   return (
     <React.Fragment>
-      <AppComponentCenter>
-        <AppComponentLogoContainer>
-          <AppComponentLogoLogo src={logo} />
-        </AppComponentLogoContainer>
-      </AppComponentCenter>
-      <ContainerApp>
-        <ContainerAppOne>
-          <AppOnePartOne>
-            <PartOneSpan>Bill</PartOneSpan>
-            <PartOneDiv>
-              <PartOneImg src={dollar} alt="dolar" />
-              <PartOneInput type="text" />
-            </PartOneDiv>
-          </AppOnePartOne>
+      <AppAll>
+        <AppCenter>
+          <AppComponentCenter>
+            <AppComponentLogoContainer>
+              <AppComponentLogoLogo src={logo} />
+            </AppComponentLogoContainer>
+          </AppComponentCenter>
+          <ContainerApp>
+            <ContainerAppOne>
+              <AppOnePartOne>
+                <PartOneSpan>Bill</PartOneSpan>
+                <PartOneDiv>
+                  <PartOneImg src={dollar} alt="dolar" />
+                  <PartOneInput
+                    type="text"
+                    value={billState}
+                    onKeyDown={(e) => handleKey(e)}
+                  />
+                </PartOneDiv>
+              </AppOnePartOne>
 
-          <AppOnePartTwo>
-            <PartTwoSpan>Select Tip %</PartTwoSpan>
-            <PartTwoDiv>
-              <PartTwoButton>5%</PartTwoButton>
-              <PartTwoButton>10%</PartTwoButton>
-              <PartTwoButton>15%</PartTwoButton>
-              <PartTwoButton>25%</PartTwoButton>
-              <PartTwoButton>50%</PartTwoButton>
-              <PartTwoInput type="text" />
-            </PartTwoDiv>
-          </AppOnePartTwo>
+              <AppOnePartTwo>
+                <PartTwoSpan>Select Tip %</PartTwoSpan>
+                <PartTwoDiv>
+                  <>
+                    {tipRender.map((item) =>
+                      !item.active ? (
+                        <PartTwoButton
+                          key={item.numberSelect}
+                          onClick={() => handleClick(item.numberSelect)}
+                        >
+                          {item.numberSelect}%
+                        </PartTwoButton>
+                      ) : (
+                        <PartTwoButtonActive key={item.numberSelect}>
+                          {item.numberSelect}%
+                        </PartTwoButtonActive>
+                      )
+                    )}
+                  </>
+                  <PartTwoInput type="text" placeholder="Custom" />
+                </PartTwoDiv>
+              </AppOnePartTwo>
 
-          <AppOnePartOne>
-            <PartOneSpan>Number of people</PartOneSpan>
-            <PartOneDiv>
-              <PartOneImg src={person} alt="person" />
-              <PartOneInput type="text" />
-            </PartOneDiv>
-          </AppOnePartOne>
-        </ContainerAppOne>
-        <ContainerAppTwo>
-          <AppTwoPartOne>
-            <ATPartOneBoxOne>
-              <ATPartOneBoxOneTitle>Tip Amount</ATPartOneBoxOneTitle>
-              <ATPartOneBoxOneDescription>/ person</ATPartOneBoxOneDescription>
-            </ATPartOneBoxOne>
-            <ATPartOneBoxTwoPrice>$00.00</ATPartOneBoxTwoPrice>
-          </AppTwoPartOne>
-          <AppTwoPartOne>
-            <ATPartOneBoxOne>
-              <ATPartOneBoxOneTitle>Total</ATPartOneBoxOneTitle>
-              <ATPartOneBoxOneDescription>/ person</ATPartOneBoxOneDescription>
-            </ATPartOneBoxOne>
-            <ATPartOneBoxTwoPrice>$00.00</ATPartOneBoxTwoPrice>
-          </AppTwoPartOne>
-          <AppTwoPartEnd>RESET</AppTwoPartEnd>
-        </ContainerAppTwo>
-      </ContainerApp>
+              <AppOnePartOne>
+                <PartOneSpan>Number of people</PartOneSpan>
+                <PartOneDiv>
+                  <PartOneImg src={person} alt="person" />
+                  <PartOneInput
+                    type="text"
+                    value={peopleState}
+                    onKeyDown={(e) => handleKeyTwo(e)}
+                  />
+                </PartOneDiv>
+              </AppOnePartOne>
+            </ContainerAppOne>
+            <ContainerAppTwo>
+              <AppTwoPartOne>
+                <ATPartOneBoxOne>
+                  <ATPartOneBoxOneTitle>Tip Amount</ATPartOneBoxOneTitle>
+                  <ATPartOneBoxOneDescription>
+                    / person
+                  </ATPartOneBoxOneDescription>
+                </ATPartOneBoxOne>
+                <ATPartOneBoxTwoPrice>$00.00</ATPartOneBoxTwoPrice>
+              </AppTwoPartOne>
+              <AppTwoPartOne>
+                <ATPartOneBoxOne>
+                  <ATPartOneBoxOneTitle>Total</ATPartOneBoxOneTitle>
+                  <ATPartOneBoxOneDescription>
+                    / person
+                  </ATPartOneBoxOneDescription>
+                </ATPartOneBoxOne>
+                <ATPartOneBoxTwoPrice>$00.00</ATPartOneBoxTwoPrice>
+              </AppTwoPartOne>
+              <AppTwoPartEnd>RESET</AppTwoPartEnd>
+            </ContainerAppTwo>
+          </ContainerApp>
+        </AppCenter>
+      </AppAll>
     </React.Fragment>
   );
 }
--- src/components/Styles.tsx
+++ src/components/Styles.tsx
@@ -1,30 +1,147 @@
 import styled from "styled-components";
 
-export const AppComponentCenter = styled.div``;
+export const AppAll = styled.div`
+  width: 100vw;
+  height: 100vh;
+  overflow: auto;
+  display: grid;
+`;
+export const AppCenter = styled.div`
+  width: 100%;
+  max-width: 1200px;
+`;
+
+export const AppComponentCenter = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 3rem 0;
+`;
 export const AppComponentLogoContainer = styled.nav``;
 export const AppComponentLogoLogo = styled.img``;
 
-export const ContainerApp = styled.div``;
+export const ContainerApp = styled.div`
+  padding: 1.5rem;
+  background: var(--white);
+  border-radius: 1rem;
+`;
 
-export const ContainerAppOne = styled.div``;
+export const ContainerAppOne = styled.div`
+  width: 100%;
+`;
 
-export const AppOnePartOne = styled.div``;
-export const PartOneSpan = styled.span``;
-export const PartOneDiv = styled.div``;
-export const PartOneImg = styled.div``;
-export const PartOneInput = styled.input``;
+export const AppOnePartOne = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  margin: 1rem 0;
+`;
+export const PartOneSpan = styled.span`
+  font-size: 1rem;
+  color: var(--dark-grayish-cyan);
+`;
+export const PartOneDiv = styled.div`
+  display: flex;
+  position: relative;
+  width: 100%;
+  background: var(--very-light-grayish-cyan);
+  padding: 0.5rem;
+  border-radius: 0.5em;
+  margin: 0.5rem 0 1rem 0;
+`;
+export const PartOneImg = styled.img`
+  position: absolute;
+  bottom: 0;
+  margin: auto 0.5rem;
+  top: 0;
+`;
+export const PartOneInput = styled.input`
+  border: none;
+  background: transparent;
+  width: 100%;
+  color: var(--very-dark-cyan);
+  font-size: 1.5rem;
+  text-align: right;
+  outline: none;
+`;
 
 export const AppOnePartTwo = styled.div``;
-export const PartTwoSpan = styled.span``;
-export const PartTwoDiv = styled.div``;
-export const PartTwoButton = styled.button``;
-export const PartTwoInput = styled.input``;
+export const PartTwoSpan = styled.span`
+  font-size: 1rem;
+  color: var(--dark-grayish-cyan);
+`;
+export const PartTwoDiv = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 45%);
+  gap: 5%;
+  margin: 1rem 0 2.5rem 0;
+  height: 11rem;
+  grid-template-rows: repeat(3, 3.3rem);
+`;
+export const PartTwoButton = styled.button`
+  width: 100%;
+  font-size: 1.5rem;
+  background: var(--very-dark-cyan);
+  color: var(--white);
+  border-radius: 0.2rem;
+  padding: 0.4rem;
+`;
+export const PartTwoButtonActive = styled.button`
+  width: 100%;
+  font-size: 1.5rem;
+  background: var(--strong-cyan);
+  color: var(--very-dark-cyan);
+  border-radius: 0.2rem;
+  padding: 0.4rem;
+`;
 
-export const ContainerAppTwo = styled.div``;
+export const PartTwoInput = styled.input`
+  width: 100%;
+  font-size: 1.5rem;
+  background: var(--very-light-grayish-cyan);
+  border-radius: 0.2rem;
+  padding: 0.4rem;
+  border: none;
+  outline: none;
+  color: var(--very-dark-cyan);
+  text-align: center;
+`;
 
-export const AppTwoPartOne = styled.div``;
-export const ATPartOneBoxOne = styled.div``;
-export const ATPartOneBoxOneTitle = styled.h5``;
-export const ATPartOneBoxOneDescription = styled.span``;
-export const ATPartOneBoxTwoPrice = styled.h4``;
-export const AppTwoPartEnd = styled.button``;
+export const ContainerAppTwo = styled.div`
+  background: var(--very-dark-cyan);
+  border-radius: 0.8rem;
+  padding: 2rem 1.5rem;
+  gap: 2rem;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const AppTwoPartOne = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+export const ATPartOneBoxOne = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+export const ATPartOneBoxOneTitle = styled.h5`
+  font-size: 1rem;
+  color: var(--white);
+`;
+export const ATPartOneBoxOneDescription = styled.span`
+  color: var(--grayish-cyan);
+  font-size: 0.8rem;
+`;
+export const ATPartOneBoxTwoPrice = styled.h4`
+  color: var(--strong-cyan);
+  font-size: 2rem;
+`;
+export const AppTwoPartEnd = styled.button`
+  width: 100%;
+  background: var(--strong-cyan);
+  color: var(--very-dark-cyan);
+  padding: 0.8rem;
+  border-radius: 0.3rem;
+  font-size: 1rem;
+`;
